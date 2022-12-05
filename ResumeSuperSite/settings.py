2d465d559dea95cc15bc9d511ac520ce1715089a
Kamil <50270585+NinevskiyK@users.noreply.github.com>
2022-12-05 21:04:37 +0300

    Update settings.py

--- ResumeSuperSite/settings.py
+++ ResumeSuperSite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5o-f42!2q(6ntm_$bv6-stw^roxw*+r9u$^&hu6mxy_0r!_69$'
+SECRET_KEY = 'Kamil is genius'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
